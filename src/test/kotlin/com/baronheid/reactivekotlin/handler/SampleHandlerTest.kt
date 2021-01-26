563bc21a3623425831edb4100c456fbe50cd1a6f
George Baronheid <baronheid.george@gmail.com>
2021-01-26 08:28:42 -0300

    Tests

--- src/test/kotlin/com/baronheid/reactivekotlin/handler/SampleHandlerTest.kt
+++ src/test/kotlin/com/baronheid/reactivekotlin/handler/SampleHandlerTest.kt
@@ -1,16 +1,19 @@
 package com.baronheid.reactivekotlin.handler
 
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.EntityExchangeResult
 import org.springframework.test.web.reactive.server.WebTestClient
-import reactor.core.publisher.Flux
+import org.springframework.test.web.reactive.server.WebTestClient.BodySpec
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityResultHandler
 import reactor.kotlin.core.publisher.toMono
 import reactor.kotlin.test.test
+import java.util.function.Consumer
+
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -20,10 +23,10 @@ class SampleHandlerTest {
     private lateinit var webTestClient: WebTestClient
 
     @Test
-    @Disabled
+//    @Disabled
     fun `Flux approach test`() {
 
-        val integerFlux = webTestClient.get().uri("functional/mono")
+        val integerFlux = webTestClient.get().uri("/functional/flux")
                 .accept(MediaType.APPLICATION_NDJSON)
                 .exchange()
                 .expectStatus().isOk
@@ -34,15 +37,5 @@ class SampleHandlerTest {
                 .expectSubscription()
                 .expectNextCount(4)
                 .verifyComplete()
-    }
-
-    @Test
-    fun `Mono endpoint test`(){
-
-        val expectedInt = 1.toMono()
-
-        webTestClient.get().uri("/functional/mono")
-
-
     }
 }
